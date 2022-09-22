57322d227fc76a7275ebf109a5cb91391dffd9e1
Adophilus <uchenna19of@gmail.com>
2022-09-22 18:41:28 +0100

    beginning of debug branch

--- build/server.js
+++ build/server.js
@@ -28,6 +28,7 @@ export default class extends Server {
         for (const file of files) {
             this.logger.info(`Loading app: '${file}'`);
             const _module = await import(`../${path.join(APPS_PATH, file)}`);
+            this.logger.info(_module);
             this.app.use(`/${file}`, _module.app);
         }
     }
