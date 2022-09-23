f77a0a969e5c1d72d22c1f00ca7f4a9f44d9a731
Adophilus <uchenna19of@gmail.com>
2022-09-23 21:33:09 +0100

    modded ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,6 @@ module.exports = {
   apps: [
     {
       script: './build/main.js',
-      watch: true,
-      ignore_watch: ['./**/node_modules'],
       node_args: "--experimental-specifier-resolution=node"
     },
     {
