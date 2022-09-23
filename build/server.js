c2018b06c3381179a063edfbd05fc161c3c6f8aa
Adophilus <uchenna19of@gmail.com>
2022-09-23 08:37:49 +0100

    updated build

--- build/server.js
+++ build/server.js
@@ -28,8 +28,8 @@ export default class extends Server {
         for (const file of files) {
             this.logger.info(`Loading app: '${file}'`);
             const _module = await import(`../${path.join(APPS_PATH, file)}`);
-            this.logger.info(_module);
             this.app.use(`/${file}`, _module.app);
+            this.logger.info(`Loaded app: '${file}'`);
         }
     }
     start(port = parseInt(process.env.PORT ?? '8000'), host = process.env.HOST ?? '0.0.0.0') {
