5551013d33059f64de6ed7b694e309872cf8357d
Adophilus <uchenna19of@gmail.com>
2022-09-23 22:34:47 +0100

    altered debug msgs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,6 @@ module.exports = {
     {
       script: './build/main.js',
       node_args: "--experimental-specifier-resolution=node"
-    },
-    {
-      name: "start apps",
-      cwd: "./",
-      script: "./bin/start-apps.sh",
-      exec_interpreter: "bash"
     }
   ]
 }
