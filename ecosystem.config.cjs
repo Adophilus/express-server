004f0e800378c24e71420de8dd4506868f398286
Adophilus <uchenna19of@gmail.com>
2022-09-29 10:05:16 +0100

    added port mangling

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   apps: [
     {
+      script: './bin/start-apps-extra',
+      interpreter: 'bash'
+    },
+    {
       script: './build/main.js',
       node_args: "--experimental-specifier-resolution=node"
     }
