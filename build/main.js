730ad73746414910cb51f682cb2f5691d68bdcc9
Adophilus <uchenna19of@gmail.com>
2022-09-29 15:13:54 +0100

    add 30 sec delay

--- build/main.js
+++ build/main.js
@@ -1,5 +1,12 @@
 import * as dotenv from 'dotenv';
 import Server from './server.js';
 dotenv.config();
+function sleep(time) {
+    var stop = new Date().getTime();
+    while (new Date().getTime() < stop + time) {
+        ;
+    }
+}
+sleep(30 * 1000);
 const server = new Server();
 server.start();
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,14 @@ import Server from './server.js'
 
 dotenv.config()
 
+function sleep(time:number) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+}
+
+sleep(30 * 1000)
+
 const server = new Server()
 server.start()
