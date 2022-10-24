06186c674b078cbea21b27ff8195c25a87ceb7f8
Adophilus <uchenna19of@gmail.com>
2022-10-24 21:28:17 +0100

    removed start-apps-extra script in pm2 ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,6 @@
 module.exports = {
   apps: [
     {
-      script: './bin/start-apps-extra',
-      instances: 1,
-      autorestart: false,
-      interpreter: 'bash'
-    },
-    {
       script: './build/main.js',
       node_args: "--experimental-specifier-resolution=node"
     }
